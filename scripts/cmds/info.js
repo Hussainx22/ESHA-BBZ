89e58a7320697a2b690ff82387280cec371d0a6b
Hussainx22 <botx2025@gmail.com>
2025-01-06 06:13:26 +0000

    Create info.js

---
+++ scripts/cmds/info.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const moment = require('moment-timezone');
+
+module.exports = {
+	config: {
+		name: "info",
+		version: "1.0",
+		author: "NTKhang",
+		countDown: 20,
+		role: 0,
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
+	},
+	onStart: async function ({ message }) {
+		const authorName = " ⩸𝑯𝑼𝑺𝑺𝑨𝑰𝑵⩸ ";
+		const ownAge = "『 ⩸__16__⩸ 』";
+		const messenger = "𝑫𝒆𝒎𝒖 𝒏𝒂";
+		const authorFB = "https://www.facebook.com/profile.php?id=100071009500533&mibextid=ZbWKwL";
+		const authorNumber = "_01965142856";
+		const Status = "⩸__🆂🅸🅽🅶🅻🅴__⩸";
+		const urls = [
+"https://i.imgur.com/wNQHWAk.mp4",
+"https://i.imgur.com/PrkdFrV.mp4",
+"https://i.imgur.com/KQ1kOo2.mp4",
+"https://i.imgur.com/z3smEma.mp4"
+];
+		const link = urls[Math.floor(Math.random() * urls.length)];
+		const now = moment().tz('Asia/Jakarta');
+		const date = now.format('MMMM Do YYYY');
+		const time = now.format('h:mm:ss A');
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+		message.reply({
+			body: `💫《 ⩸__𝐁𝐨𝐭 𝐀𝐧𝐝 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧__⩸ 》💫
+\🤖彡𝐵𝑜𝑡 𝑁𝑎𝑚𝑒 : ⩸__${global.GoatBot.config.nickNameBot}__⩸
+\👾彡𝐵𝑜𝑡 𝑆𝑦𝑠𝑡𝑒𝑚 𝑃𝑟𝑒𝑓𝑖𝑥 : ${global.GoatBot.config.prefix}
+\💙彡𝑂𝑤𝑛𝑒𝑟 𝑁𝑎𝑚𝑒 : ${authorName}
+\📝彡𝐴𝑔𝑒  : ${ownAge}
+\💕彡𝑅𝑒𝑙𝑎𝑡𝑖𝑜𝑛𝑆ℎ𝑖𝑝: ${Status}
+\🌐彡𝑊𝑝 : ${authorNumber}
+\🌍彡𝐹𝑎𝑐𝑒𝑏𝑜𝑜𝑘 𝐿𝑖𝑛𝑘 : ${authorFB}
+\🗓彡𝐷𝑎𝑡𝑒 : ${date}
+\⏰彡𝑁𝑜𝑤 𝑇𝑖𝑚𝑒 : ${time}
+\🔰彡𝐴𝑛𝑦 𝐻𝑒𝑙𝑝 𝐶𝑜𝑛𝑡𝑎𝑐𝑡 :⩸__${messenger}__⩸
+\📛彡𝐵𝑜𝑡 𝐼𝑠 𝑅𝑢𝑛𝑛𝑖𝑛𝑔 𝐹𝑜𝑟 : ${uptimeString}
+    𝑻𝒈: 😒
+    𝑰𝒏𝒔𝒕𝒂: 𝑲𝒊 𝒌𝒐𝒓𝒃𝒂?
+    𝑪𝒂𝒑𝑪𝒖𝒕: 𝑯𝒖𝒔𝒔𝒂𝒊𝒏 𝒙2
+    𝑻𝒊𝒌𝑻𝒐𝒌: 🫣?
+    𝒀𝒐𝒖𝑻𝒖𝒃𝒆:  𝑨𝒊 𝒈𝒖𝒍𝒂 𝒂𝒎𝒂𝒓 𝒅𝒂𝒓𝒂 𝒉𝒐𝒊𝒃𝒐 𝒏𝒂🫨
+\===============`,
+			attachment: await global.utils.getStreamFromURL(link)
+		});
+	},
+	onChat: async function ({ event, message, getLang }) {
+		if (event.body && event.body.toLowerCase() === "info") {
+			this.onStart({ message });
+		}
+	}
+};
